2d9f6e9c3e9d934a7e7dcf1430c713b881aa9718
ysh <livefor1212@gmail.com>
2021-07-13 23:50:41 +0900

    block add

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -104,6 +104,11 @@ public class BasicController {
         return "basic/comments";
     }
 
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
     @Component("helloBean")
     static class HelloBean{
         public String hello(String data){
